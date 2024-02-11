d1106d14767351ea1b56229d3ba57fefad9516d4
Buckwich <Buckwich@users.noreply.github.com>
2024-02-11 22:27:41 +0100

    add qmk2kcf converter

--- libs/converter/src/lib/converter.ts
+++ libs/converter/src/lib/converter.ts
@@ -1,3 +1,42 @@
-export function converter(): string {
-  return 'converter';
+import { KCFKeyboard, QMKKeyboard } from '@keyboard-helper/keyboard-schema';
+
+export class KCFConverter {
+  public keyboard: KCFKeyboard;
+
+  constructor(kfcKeyboard?: KCFKeyboard, qmkKeyboard?: QMKKeyboard) {
+    this.keyboard = {
+      layout: [],
+    };
+    if (kfcKeyboard) {
+      this.keyboard = kfcKeyboard;
+    }
+
+    if (qmkKeyboard) {
+      const importedKeyboard = KCFConverter.qmkToKfc(qmkKeyboard);
+      this.keyboard = importedKeyboard;
+    }
+  }
+  public static qmkToKfc(qmkKeyboard: QMKKeyboard): KCFKeyboard {
+    const kfcKeyboard: KCFKeyboard = {
+      layout: [],
+    };
+    const layout = qmkKeyboard.layouts?.[0].layout;
+    if (layout) {
+      layout.forEach((key) => {
+        kfcKeyboard.layout.push({
+          x: key.x,
+          y: key.y,
+          w: key.w ?? 1,
+          h: key.h ?? 1,
+          r: key.r,
+          rx: key.rx,
+          ry: key.ry,
+          labels: {
+            mc: { text: key.label || '' },
+          },
+        });
+      });
+    }
+    return kfcKeyboard;
+  }
 }
