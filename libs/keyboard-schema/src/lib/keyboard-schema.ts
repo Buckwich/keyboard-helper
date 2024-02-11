cd9b2b8dfadb79107c14f370822373670469fddf
Buckwich <Buckwich@users.noreply.github.com>
2024-02-11 21:51:22 +0100

    add schema

--- libs/keyboard-schema/src/lib/keyboard-schema.ts
+++ libs/keyboard-schema/src/lib/keyboard-schema.ts
@@ -1,3 +1,68 @@
-export function keyboardSchema(): string {
-  return 'keyboard-schema';
+export interface KCFKeyboard {
+  layout: KCFKey[];
+}
+
+export interface KCFKey {
+  /*Absolute position of the key in the horizontal axis, in key units.*/
+  x: number;
+  /*Absolute position of the key in the vertical axis, in key units.*/
+  y: number;
+  /*The width of the key, in key units.*/
+  w: number;
+  /*The height of the key, in key units.*/
+  h: number;
+
+  /*The absolute position of the second rectangle in the horizontal axis, in key units.*/
+  x2?: number;
+  /*The absolute position of the second rectangle in the vertical axis, in key units.*/
+  y2?: number;
+  /*The width of the second rectangle, in key units.*/
+  w2?: number;
+  /*The height of the second rectangle, in key units.*/
+  h2?: number;
+
+  /*The rotation angle in degrees (0-360).*/
+  r?: number;
+  /*The absolute X position of the rotation point.*/
+  rx?: number;
+  /*The absolute Y position of the rotation point.*/
+  ry?: number;
+
+  labels: KCFLabels;
+}
+
+export interface KCFLabels {
+  /*top left*/
+  tl?: KCFLabel;
+  /*top center*/
+  tc?: KCFLabel;
+  /*top right*/
+  tr?: KCFLabel;
+
+  /*middle left*/
+  ml?: KCFLabel;
+  /*middle center*/
+  mc?: KCFLabel;
+  /*middle right*/
+  mr?: KCFLabel;
+
+  /*bottom left*/
+  bl?: KCFLabel;
+  /*bottom center*/
+  bc?: KCFLabel;
+  /*bottom right*/
+  br?: KCFLabel;
+
+  /*front left*/
+  fl?: KCFLabel;
+  /*front center*/
+  fc?: KCFLabel;
+  /*front right*/
+  fr?: KCFLabel;
+}
+
+export interface KCFLabel {
+  color?: string;
+  size?: number;
+  text: string;
 }
