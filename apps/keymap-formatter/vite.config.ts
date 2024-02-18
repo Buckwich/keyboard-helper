6f8b291503ec40112ce7a8c4cb4b1bf5b14fd4e5
Buckwich <Buckwich@users.noreply.github.com>
2024-02-18 21:54:06 +0100

    use formatter as main page

--- apps/keymap-formatter/vite.config.ts
+++ apps/keymap-formatter/vite.config.ts
@@ -5,7 +5,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   root: __dirname,
-  base: 'keymap-formatter',
+  // base: 'keymap-formatter',
   cacheDir: '../../node_modules/.vite/apps/keymap-formatter',
 
   server: {
